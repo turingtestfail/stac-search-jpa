b3763f41b6b7c460749873e65ada496098d3b572
Joseph Miller <millerj@pixia.com>
2020-08-17 10:53:14 -0400

    initial commit

---
+++ src/main/java/com/jm/stacsearchjpa/StacSearchJpaApplication.java
@@ -0,0 +1,13 @@
+package com.jm.stacsearchjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StacSearchJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StacSearchJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jm/stacsearchjpa/StacSearchJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jm.stacsearchjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StacSearchJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
