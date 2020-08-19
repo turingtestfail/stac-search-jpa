122bf4d612867e274e1288e2ad0a4c38723798f4
Joseph Miller <millerj@pixia.com>
2020-08-19 15:46:34 -0400

    added collection specific repo and endpoint

--- src/main/java/com/jm/stacsearchjpa/controller/CapabilitiesController.java
+++ src/main/java/com/jm/stacsearchjpa/controller/CapabilitiesController.java
@@ -1,21 +1,28 @@
 package com.jm.stacsearchjpa.controller;
 
 import com.jm.stacsearchjpa.model.Catalog;
+import com.jm.stacsearchjpa.model.Collection;
 import com.jm.stacsearchjpa.model.Collections;
 import com.jm.stacsearchjpa.model.repository.CatalogRepository;
+import com.jm.stacsearchjpa.model.repository.CollectionRepository;
 import com.jm.stacsearchjpa.model.repository.CollectionsRepository;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class CapabilitiesController {
     private final CatalogRepository catalogRepository;
     private final CollectionsRepository collectionsRepository;
-    public CapabilitiesController(CatalogRepository catalogRepository, CollectionsRepository collectionsRepository){
+    private final CollectionRepository collectionRepository;
+    public CapabilitiesController(CatalogRepository catalogRepository, CollectionsRepository collectionsRepository,
+                                  CollectionRepository collectionRepository){
         this.catalogRepository=catalogRepository;
         this.collectionsRepository=collectionsRepository;
+        this.collectionRepository=collectionRepository;
     }
     @GetMapping("/")
     public Catalog landingPage() {
@@ -26,6 +33,12 @@ public class CapabilitiesController {
     public Collections collections() {
         Collections collections= collectionsRepository.findFirstByOrderByIdAsc();
         return collections;
+    }
+
+    @GetMapping("/collections/{collectionId}")
+    public Collection collectionById(@PathVariable String collectionId) {
+        Optional<Collection> collection= collectionRepository.findById(collectionId);
+        return collection.get();
     }
 
 }
---
+++ src/main/java/com/jm/stacsearchjpa/model/repository/CollectionRepository.java
@@ -0,0 +1,7 @@
+package com.jm.stacsearchjpa.model.repository;
+
+import com.jm.stacsearchjpa.model.Collection;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CollectionRepository extends JpaRepository<Collection,String> {
+}
