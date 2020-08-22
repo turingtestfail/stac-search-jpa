ae9d5423686daec34b921cb0e9562c959bf05c49
Joseph Miller <millerj@pixia.com>
2020-08-22 17:59:09 -0400

    first pass at bbox search

---
+++ src/main/java/com/jm/stacsearchjpa/controller/SearchController.java
@@ -0,0 +1,53 @@
+package com.jm.stacsearchjpa.controller;
+
+import com.jm.stacsearchjpa.model.Feature;
+import com.jm.stacsearchjpa.model.FeatureCollection;
+import com.jm.stacsearchjpa.model.repository.FeatureRepository;
+import org.geotools.geometry.jts.JTSFactoryFinder;
+import org.geotools.referencing.CRS;
+import org.locationtech.jts.geom.Coordinate;
+import org.locationtech.jts.geom.CoordinateXY;
+import org.locationtech.jts.geom.Geometry;
+import org.locationtech.jts.geom.GeometryFactory;
+import org.locationtech.jts.geom.Polygon;
+import org.opengis.referencing.FactoryException;
+import org.opengis.referencing.crs.CoordinateReferenceSystem;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+import java.util.List;
+
+@RestController
+public class SearchController {
+    private final FeatureRepository featureRepository;
+    public SearchController(FeatureRepository featureRepository){
+        this.featureRepository=featureRepository;
+    }
+    @GetMapping("/search")
+    public FeatureCollection searchByGet(@RequestParam(required = false)double[]bbox) throws FactoryException {
+        FeatureCollection featureCollection = new FeatureCollection();
+        GeometryFactory geometryFactory = JTSFactoryFinder.getGeometryFactory( null );
+
+        Polygon polygonFromCoordinates = geometryFactory.createPolygon(coordinatesFromBbox(bbox));
+        polygonFromCoordinates.setSRID(4326);
+        List<Feature> features = featureRepository.customByGeometry(polygonFromCoordinates);
+        featureCollection.setFeatures(features);
+        featureCollection.setType("FeatureCollection");
+        featureCollection.setTimeStamp(new Date());
+        featureCollection.setNumberMatched(features.size());
+        featureCollection.setNumberReturned(features.size());
+        //featureCollection.setLinks(populateFeatureCollectionNextLink(collectionId));
+        return featureCollection;
+
+    }
+
+    private Coordinate[] coordinatesFromBbox(double[] bbox) {
+        Coordinate coordinateLL = new CoordinateXY(bbox[0],bbox[1]); //minx, miny
+        Coordinate coordinateUL = new CoordinateXY(bbox[0],bbox[3]); //minx, maxy
+        Coordinate coordinateUR = new CoordinateXY(bbox[2],bbox[3]); //maxx, maxy
+        Coordinate coordinateLR = new CoordinateXY(bbox[2],bbox[1]); //maxx, miny
+        return new Coordinate[]{coordinateLL,coordinateUL,coordinateUR,coordinateLR,coordinateLL};
+    }
+}
--- src/main/java/com/jm/stacsearchjpa/model/repository/FeatureRepository.java
+++ src/main/java/com/jm/stacsearchjpa/model/repository/FeatureRepository.java
@@ -1,11 +1,15 @@
 package com.jm.stacsearchjpa.model.repository;
 
 import com.jm.stacsearchjpa.model.Feature;
+import org.locationtech.jts.geom.Geometry;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface FeatureRepository extends JpaRepository<Feature,String> {
     List<Feature> findAllByCollection_Id(String collectionId);
     Feature findByCollection_IdAndId(String collectionId,String id);
+    @Query("select f from Feature f where intersects(f.geometry, ?1) = true")
+    List<Feature>customByGeometry(Geometry geometry);
 }
